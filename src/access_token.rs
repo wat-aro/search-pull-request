cc04082a1adad008030ac019c0b9cd5154248c5e
wat-aro <kazutas1008@gmail.com>
2021-01-21 00:54:35 +0900

    Deserialize ErrorCode for AccessToken

--- src/access_token.rs
+++ src/access_token.rs
@@ -7,23 +7,87 @@ pub struct Params<'a> {
     pub grant_type: &'a str,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Deserialize)]
 pub struct SuccessResponse {
     access_token: String,
     token_type: String,
     scope: String,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Deserialize)]
 pub struct ErrorResponse {
     error_description: String,
-    error: String,
+    error: ErrorCode,
     error_uri: String,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Deserialize)]
 #[serde(untagged)]
 pub enum Response {
     Success(SuccessResponse),
     Err(ErrorResponse),
+}
+
+#[derive(Debug, Deserialize, PartialEq)]
+#[serde(rename_all = "snake_case")]
+enum ErrorCode {
+    AuthorizationPending,
+    SlowDown,
+    ExpiredToken,
+    UnsupportedGrantType,
+    IncorrectClientCredentials,
+    IncorrectDeviceCode,
+    AccessDenied,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use serde_json;
+
+    #[test]
+    fn serialize_params() {
+        let params = Params {
+            client_id: "client_id",
+            device_code: "device_code",
+            grant_type: "grant_type",
+        };
+
+        if let Ok(json) = serde_json::to_string(&params) {
+            assert_eq!(json, "{\"client_id\":\"client_id\",\"device_code\":\"device_code\",\"grant_type\":\"grant_type\"}");
+        }
+    }
+
+    #[test]
+    fn deserialize_error_code() {
+        let json = r#"
+            [
+              "authorization_pending",
+              "slow_down",
+              "expired_token",
+              "unsupported_grant_type",
+              "incorrect_client_credentials",
+              "incorrect_device_code",
+              "access_denied"
+            ]
+        "#;
+
+        match serde_json::from_str::<Vec<ErrorCode>>(json) {
+            Ok(error_code) => {
+                assert_eq!(
+                    error_code,
+                    vec![
+                        ErrorCode::AuthorizationPending,
+                        ErrorCode::SlowDown,
+                        ErrorCode::ExpiredToken,
+                        ErrorCode::UnsupportedGrantType,
+                        ErrorCode::IncorrectClientCredentials,
+                        ErrorCode::IncorrectDeviceCode,
+                        ErrorCode::AccessDenied,
+                    ]
+                );
+            }
+            Err(err) => panic!(err),
+        }
+    }
 }
